a698d11a0fe3cde7aa6bd38d799efd9007d2c68d
SebasValle <oscar.vallejo@iteso.mx>
2022-04-28 00:07:06 -0500

    Sonido frecuencia variante

---
+++ Top.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:37:03 04/20/2022 
+// Design Name: 
+// Module Name:    Top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Top(
+		input PS2D,
+		input clk,
+		output reg NOTE
+    );
+	 wire[25:0] note;
+	 reg [25:0] count;
+	 Receiver receiver(.ps2d(PS2D), .CLK(clk), .FinalNote(note));
+	 
+	 always @(posedge clk) begin
+		begin
+			count <= count + 1;
+			if(count == note) begin
+				NOTE <= !NOTE; //Tooglear ClkRedu
+				count <= 0;
+			end
+		end
+	 end
+endmodule
